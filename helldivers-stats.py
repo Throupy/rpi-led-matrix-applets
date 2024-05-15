e4c9378efca7ec9dcc643c33a9a3bf06b652a20f
Owen <owenthroup@gmail.com>
2024-05-15 18:50:51 +0100

    fixed sytax err

--- helldivers-stats.py
+++ helldivers-stats.py
@@ -9,7 +9,7 @@ options.rows = 64
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'adafruit-ha
+options.hardware_mapping = 'adafruit-hat'
 matrix = RGBMatrix(options=options)
 
 def load_and_convert_image(image_path):
